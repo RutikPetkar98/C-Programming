bccbbb942cd6b05f060d37222bfbfc463bbc9483
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-01-02 15:03:51 +0530

    all relationl operator add

--- Day 2 Operator/Relational_Operator.c
+++ Day 2 Operator/Relational_Operator.c
@@ -1,8 +1,42 @@
 #include<stdio.h>
 
 int main(){
-
+/**
+ * ==
+ * !=
+ * > greater than
+ * < less than
+ * <= less than
+ * >= greater than
+ */
     
+    int a=10;
+    int b=20;
+    int c=10;
+    //equal to Operator(==)
+    printf("sum=%d\n",a==b);
+    printf("sum=%d\n",a==c);
+
+    //Not equal to Operator(!=)
+    printf("sum=%d\n",a!=b);
+    printf("sum=%d\n",a!=c);
+
+    //Geater than(>)
+    print("sum=%d\n",a>b);
+    printf("sum=%d\n",a>c);
+
+    //Less than(<)
+    printf("sum=%d\n",a<b);
+    printf("sum=%d\n",a<c);
+
+    //Greater than equal to 
+    printf("sum=%d\n",a>=b);
+    printf("sum=%d\n",a>=c);
+
+    //Less than equal to 
+    printf("sum=%d\n",a<=b);
+    printf("sum=%d\n",a<=c);
+
     
 
     return 0;
