3b32226adb0c7a6c8e6d6e3e7cd3a456f672cdb4
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-01-05 14:27:11 +0530

    add if_else

---
+++ Day 3 Conditional_Statment/if_else_conditon.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Check number is Odd or Even:");
+    scanf("%d",&num);
+    if (num%2==0)
+    {
+        printf("Num=%d is even\n",num);
+    }
+    else
+    {
+         printf("Num=%d is odd\n",num);
+    }
+    printf("exiting....");
+    
+
+
+    return 0;
+
+}
