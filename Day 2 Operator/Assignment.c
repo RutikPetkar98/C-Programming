e6c1ff029cec9c9b6820be27ccd9b5c975406e10
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-01-05 10:59:37 +0530

    add assignment opertaor

--- Day 2 Operator/Assignment.c
+++ Day 2 Operator/Assignment.c
@@ -8,7 +8,43 @@
  */
 #include<stdio.h>
 int main(){
+    
+    //=
+    int a=10;
+    int b=20;
 
+    //+=
+    a=10;
+    printf("a=%d\n",a);
+    a+=5; //a=a+5
+    printf("a+=5:%d\n",a);
+
+    //-=
+    a=10;
+    printf("a=%d\n",a);
+    a-=5;//a=a-5
+    printf("a-=5:%d\n",a);
+
+    //*=
+    a=10;
+    printf("a=%d\n",a);
+    a*=5;//a=a*5
+    printf("a*=5:%d\n",a);
+
+    ///=
+    a=10;
+    printf("a=%d\n",a);
+    a/=5;//a=a/5
+    printf("a/=5:%d\n",a);
+
+    //%=
+    a=10;
+    printf("a=%d\n",a);
+    a%=5;//a=a%5
+    printf("a%=5:%d\n",a);
+
+
+    
     
     return 0;
 }
