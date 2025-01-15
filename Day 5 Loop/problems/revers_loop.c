9b89ff4fffcef548f8622fddea6762ce2d4ab662
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-01-15 17:35:14 +0530

    reverse loop

--- Day 5 Loop/problems/revers_loop.c
+++ Day 5 Loop/problems/revers_loop.c
@@ -1,18 +1,8 @@
 #include<stdio.h>
 int main(){
-int i=10;
-if ( i=0,i<=10)
-{
-    printf("%d\n",i);
-    i++;
-}
-
-
-
-// for (int i = 0; i <= 10; i++)
-// {
-//     printf("%d\n",i);
-// }
-
+printf("using for loop:\n");
+      for(int i=10;i>0;i--){
+        printf("%d\n",i);
+      }
     return 0;
 }
