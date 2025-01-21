64e3348919c3479f4fbd1fcc07afcab84930dd87
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-01-21 23:01:59 +0530

    User define function

--- Day 9 Function/User_define_fun.c
+++ Day 9 Function/User_define_fun.c
@@ -1,11 +1,11 @@
-// #include<stdio.h>
-// void fun(); //define a function 
+#include<stdio.h>
+void fun(); //define a function 
 
-// int main (){
-//     fun();   //call the function 
-//     return 0;
-// }
+int main (){
+    fun();   //call the function 
+    return 0;
+}
 
-// void fun(){ //pass a parameter in the function
-//     printf("hello coders?"); // print the function
-// }
+void fun(){ //pass a parameter in the function
+    printf("hello coders?"); // print the function
+}
