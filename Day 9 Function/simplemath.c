be3ec5b65841a9c473b95ce301d0b79f494156cb
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-01-21 18:14:41 +0530

    User define function

--- Day 9 Function/simplemath.c
+++ Day 9 Function/simplemath.c
@@ -1,32 +1,32 @@
-// #include<stdio.h>
-// #include<math.h>
-// void addition();
+#include<stdio.h>
+#include<math.h>
+void addition();
 
-// void add(int x,int y);// Function prototype(declares the function before it's used)
+void add(int x,int y);// Function prototype(declares the function before it's used)
 
-// int main(){
-//     addition();
+int main(){
+    addition();
      
-//      int a=70, // Declare and initialize the variables 'a' and 'b'
-//      int b=50;
-//      // Print the values of 'a' and 'b'
-//     printf("a=%d\n",a);
-//     printf("b=%d\n",b);
+     int a=70, // Declare and initialize the variables 'a' and 'b'
+     int b=50;
+     // Print the values of 'a' and 'b'
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
 
-//     add(a,b); // Call the add function with the variables 'a' and 'b'
+    add(a,b); // Call the add function with the variables 'a' and 'b'
 
-//     add(50,50);   // Call the add function with literal values
+    add(50,50);   // Call the add function with literal values
     
     
-//     return 0;
-// }
+    return 0;
+}
 
-// void addition(){
-//     int a=10,b=20;
-//     printf("Addition:%d\n",a+b);
-// }
+void addition(){
+    int a=10,b=20;
+    printf("Addition:%d\n",a+b);
+}
 
-// // Function definition(actual implementation of the function)
-// void add(int x,int y){
-//     printf("Add:%d\n",(x+y));
-// } 
+// Function definition(actual implementation of the function)
+void add(int x,int y){
+    printf("Add:%d\n",(x+y));
+} 
