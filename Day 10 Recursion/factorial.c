ba630a1540cfc84510e94208ea049a183c421cdd
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-02-06 13:57:03 +0530

    add factorial

--- Day 10 Recursion/factorial.c
+++ Day 10 Recursion/factorial.c
@@ -1,23 +1,23 @@
-// #include <stdio.h>
-// int fact(int n);
-// int main()
-// {
+#include <stdio.h>
+int fact(int n);
+int main()
+{
 
-//     int number;
+    int number;
 
-//     printf("Enter the number:");
-//     scanf("%d", &number);
+    printf("Enter the number:");
+    scanf("%d", &number);
 
-//     int result = fact(number);
-//     printf("Number of %d and its factorial %d\n", number, result);
-//     return 0;
-// }
+    int result = fact(number);
+    printf("Number of %d and its factorial %d\n", number, result);
+    return 0;
+}
 
-// int fact(int n)
-// {
-//     if (n == 1 || n == 0)
-//     {
-//         return 1;
-//     }
-//     return n * fact(n - 1);
-// }
+int fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
