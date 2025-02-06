1984fdb627ee3a7084aaf53b9f70654aa0df2581
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-02-06 14:20:03 +0530

    find fibonacci series

--- Day 10 Recursion/fibonacci_series.c
+++ Day 10 Recursion/fibonacci_series.c
@@ -1,11 +1,27 @@
-// #include <stdio.h>
-// int fib();
-// int main()
-// {
+#include <stdio.h>
+int fib(int n);
+int main()
+{
 
-//     if (int i = 1; i <= 10; i++)
-//     {
-//         int num = fib(i);
-//         printf("result:%d\n", num);
-//     }
-// }
+    for (int i = 0; i <= 10; i++)
+    {
+        int num = fib(i);
+        printf("fibonacci_num:%d\n", num);
+    }
+    return 0;
+}
+
+int fib(int n)
+{
+
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
