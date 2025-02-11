2e95b35c71e26235e2641864b2e140c69c5fb74a
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-02-11 19:26:18 +0530

    factorial  using recursion

---
+++ Day 10 Recursion/factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int fact(int n);
+int main()
+{
+    int number;
+    printf("Enterr the Number:-");
+    scanf("%d", &number);
+
+    int result = fact(number);
+    printf("Number of %d and its factorial %d\n", number, result);
+
+    return 0;
+}
+int fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
---
+++ Day 10 Recursion/tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int fact = 5;
+    int factoril = fact(5) * n;
+
+    printf("factoiral", factoril);
+
+    return 0;
+}
