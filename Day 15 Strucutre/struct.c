88f929eff995c37086f11d44106935f4a1fc19c9
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-02-16 17:46:42 +0530

    add user input structure data

--- Day 15 Strucutre/struct.c
+++ Day 15 Strucutre/struct.c
@@ -25,5 +25,26 @@ int main()
     printf("Name=%s\n", s1.s_name);
     printf("Marks=%lf\n", s1.s_marks);
 
+    // struct array
+    // int arr[size];
+    stud arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("enter stud_id:");
+        scanf("%d", &arr[i].s_id);
+        printf("enter stud_name:");
+        char temp[20];
+        scanf("%s", &temp);
+        strcpy(arr[i].s_name, temp);
+        printf("enter stud_marks:");
+        scanf("%lf", &arr[i].s_marks);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("stud_id:%d\n", arr[i].s_id);
+        printf("stud_name:%s\n", arr[i].s_name);
+        printf("stud_marks:%lf\n", arr[i].s_marks);
+    }
+
     return 0;
 }
