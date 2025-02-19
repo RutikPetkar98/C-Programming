380ffaa3fbf28ad98b2e7468d99ac4460e29aa91
RutikPetkar98 <rutikpetkar431@gmail.com>
2025-02-19 19:05:47 +0530

    Binary search in c

---
+++ Day 18 Searching/Binary_Search.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+
+    int arr[5] = {1, 9, 17, 18, 53};
+
+    int key = 0;
+    int size = 5;
+    int flag = 0;
+
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high)
+    {
+        printf("inside loop\n");
+        int mid = (low + high) / 2;
+        printf("mid=%d\n", mid);
+
+        if (key == arr[mid])
+        {
+            printf("element found! arr[%d]=%d\n", mid, arr[mid]);
+            flag = 1;
+            break;
+        }
+        else if (key > arr[mid])
+        {
+            low = mid + 1;
+            printf("low=%d\n", low);
+            high = high;
+            printf("high=%d\n", high);
+        }
+        else
+        {
+            low = low;
+            printf("low=%d\n", low);
+            high = mid - 1;
+            printf("high=%d\n", high);
+        }
+    }
+    if (flag == 0)
+    {
+        printf("element not found!\n");
+    }
+    printf("exist\n");
+
+    return 0;
+}
